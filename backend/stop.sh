a41c3bd76a9e490f614b281f4b41ffdbad6070a6
movptr06 <movptr06@outlook.kr>
2023-02-28 08:09:29 +0900

    RM log

--- backend/stop.sh
+++ backend/stop.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 
-mkdir log 1>/dev/null 2>/dev/null
-docker-compose logs > log/`date "+%Y-%m-%d-%H:%M:%S"`.log
+#mkdir log 1>/dev/null 2>/dev/null
+#docker-compose logs > log/`date "+%Y-%m-%d-%H:%M:%S"`.log
 docker-compose down
