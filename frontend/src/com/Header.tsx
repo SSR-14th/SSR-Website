6fc355ac1a72e6c9e1796d276c7d14aee8c105b6
movptr06 <movptr06@outlook.kr>
2023-03-02 18:34:03 +0900

    FEAT menu

--- frontend/src/com/Header.tsx
+++ frontend/src/com/Header.tsx
@@ -12,12 +12,14 @@ const Header = (props: any) => {
   const { children } = props;
   return (
     <div>
+      <img src={require('../img/SSR-logo.png')} />
       <div className="title">{children}</div>
       <div className="menu">
         <MenuItem>홈</MenuItem>
         <MenuItem>소개</MenuItem>
         <MenuItem>포스트</MenuItem>
       </div>
+      <hr/>
     </div>
   );
 };
