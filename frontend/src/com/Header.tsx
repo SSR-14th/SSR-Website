9527e7b740c9787f8c0593f56a3b74d21995ab15
movptr06 <movptr06@outlook.kr>
2023-03-02 18:10:18 +0900

    ADD menu

---
+++ frontend/src/com/Header.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Header.css";
+
+const MenuItem = (props: any) => {
+  const { children } = props;
+  return (
+    <div className="menu-item">{children}</div>
+  );
+};
+
+const Header = () => {
+  return (
+    <div>
+      <div className="menu">
+        <MenuItem>홈</MenuItem>
+        <MenuItem>소개</MenuItem>
+        <MenuItem>포스트</MenuItem>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
