b0d439ce166d15d2e90faceca507d63afa924f15
movptr06 <movptr06@outlook.kr>
2023-03-03 23:15:53 +0900

    RM footer

--- frontend/src/component/Footer/index.tsx
+++ frontend/src/component/Footer/index.tsx
@@ -9,10 +9,7 @@ export const Footer = () => {
             <footer>
                 <div className="foot">
                   <div className="footer-list">
-                    <p className="footer-text">Terms of Use</p>
-                    <p className="footer-text">Privacy Police</p>
                   </div>
-                  <p className="footer-text">© 2023 SSR. All Rights Reserved</p>
                 </div>
             </footer>
         </div>
