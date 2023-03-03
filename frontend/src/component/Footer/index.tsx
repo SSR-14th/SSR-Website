1591eea58f749602b2fc1593880dcbf283ad8f8e
movptr06 <movptr06@outlook.kr>
2023-03-03 22:23:40 +0900

    ADD footer

---
+++ frontend/src/component/Footer/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+import { Link } from 'react-router-dom';
+
+export const Footer = () => {
+    return (
+        <div>
+            <footer>
+                <div className="foot">
+                </div>
+            </footer>
+        </div>
+    );
+};
+
